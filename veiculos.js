9f21e61a3f68c0f59647a1faedc4d3c619dad4ad
leonardolon <121575621+leonardolon@users.noreply.github.com>
2023-10-09 18:37:30 -0300

    Update veiculos.js

--- veiculos.js
+++ veiculos.js
@@ -6,34 +6,27 @@ function Carro(nome, marca, preco) {
     
     this.marca = marca;
 
-    let _preco = preco;
+    this.preco= preco;
     
-    this.getPreco = function(){
-        return _preco;
-    }
-
-    this.setPreco = function(valor){
-        if (typeof valor === 'number'){
-            _preco = valor;
-        }    
-        
-    }
     
     Modelo.call(this, nome);
 }
 
 function Ford(nome){
-    carro.call(this,nome, "ford", 25000)
+    Carro.call(this,nome, "ford", 25000)
+    
 }
 
 function Toyota(nome){
-    carro.call(this,nome, "Toyota", 25000)                     
+    Carro.call(this,nome, "Toyota", 25000)  
+    
 }
         
 
-const Carro1 = new Carro("voyage, VW", 15000);
-const Carro2 = new Carro("FordKa");
-const Carro3 = new Carro("supra");
+const Carro1 = new Carro("voyage", "VW", 15000);
+const Carro2 = new Ford("FordKa");
+const Carro3 = new Toyota("supra");
+
 
 
 console.log(Carro1)
