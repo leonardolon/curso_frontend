cd9cc76dd17e268bd5dc7ff87563b5c7ef620aa6
Leonardo Oliveira <leonardo.lon@outlook.com>
2023-10-09 18:33:58 -0300

    final commit

--- veiculos.js
+++ veiculos.js
@@ -1,21 +1,34 @@
-function Carro(nome, marca, preco) {    
-    this.nome = nome;
-    this.marca = marca;
-
-    let _preco = preco;
-    
-    this.getPreco = function(){
-        return _preco;
-    }
-
-    this.setPreco = function(valor){
-        if (typeof valor === 'number'){
-            _preco = valor;
-        }    
-        
-    }
+function Modelo(nome){
+    this.nome= nome;
 }
 
-const Carro1 = new Carro("voyage, VW", 15000);
+function Carro(nome, marca, preco) {    
+    
+    this.marca = marca;
+
+    this.preco= preco;
+    
+    
+    Modelo.call(this, nome);
+}
+
+function Ford(nome){
+    Carro.call(this,nome, "ford", 25000)
+    
+}
+
+function Toyota(nome){
+    Carro.call(this,nome, "Toyota", 25000)  
+    
+}
+        
+
+const Carro1 = new Carro("voyage", "VW", 15000);
+const Carro2 = new Ford("FordKa");
+const Carro3 = new Toyota("supra");
+
+
 
 console.log(Carro1)
+console.log(Carro2)
+console.log(Carro3)
